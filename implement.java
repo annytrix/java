dc9fc3874b1305ab932e7587ea6397a9fb2ddb06
annytrix <kumarianjana5801@gmail.com>
2026-06-03 17:00:07 +0530

    conversion

---
+++ implement.java
@@ -0,0 +1,42 @@
+public class implement {
+    public static void main(String[] args) {
+        //implicit conversion
+        //(byte to int)
+        // byte b=24;
+        // int i;
+
+        // i=b;
+        // System.err.println(i);//24
+
+        // character to int
+
+        // char c = 'a';
+        // int  i;
+        // i = c;
+        // System.out.println(i);
+        // Explicit conversion
+        // int i =300;
+        // byte b;//-128 to 127
+        // b = (byte)i;
+        // System.err.println(b);//300%256=44
+        
+        // Turncating conversion
+
+        // float f= 15.678f;
+        // int i;
+        //  i = (int)f;
+        //  System.out.println(i);//15
+
+        // boolean bool = false;
+        // int i;
+        // i=bool;
+        byte b =50;
+        b = (byte) (b*2);
+        System.out.println(b);//100
+
+
+
+
+    }
+    
+}
