a6db9d71787d845c6d24cc90d09f59f0bca71a89
annytrix <kumarianjana5801@gmail.com>
2026-06-03 18:27:08 +0530

    operatoers

---
+++ operatoers.java
@@ -0,0 +1,20 @@
+public class operatoers {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = b / a;
+        int g = b % a; 
+        System.out.println(c + " ," + d+" ," + e +" ,"+ f + ","+g);
+
+        int h = a + 2;
+        // h = h + 2; Since this is okey in CSE
+         h += 2;// h=9
+         h -= 2;// h=7
+         System.err.println(h);
+
+    }
+}
