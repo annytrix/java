e4ca47c5c777a196f95b73b9f275f672158e2d1e
annytrix <kumarianjana5801@gmail.com>
2026-06-04 15:53:52 +0530

    logical

---
+++ logical.java
@@ -0,0 +1,19 @@
+public class logical {
+    public static void main(String[] args) {
+        //relational Operators.-->==, !=, <, >,<=, >=
+        int a = 10;
+        int b = 10;
+
+        boolean c = (a==b);
+        System.err.println(c);
+
+        boolean d =(a != b);
+        System.err.println(d);
+
+        boolean e = (a < b);
+        boolean f = (a <= b);
+        System.err.println(e);
+        System.err.println(f);
+    }
+    
+}
