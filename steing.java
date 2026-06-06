397368245048ca47c2bc9ff49062a2cf2ae2d608
annytrix <kumarianjana5801@gmail.com>
2026-06-06 08:59:06 +0530

    1d

---
+++ steing.java
@@ -0,0 +1,12 @@
+public class steing {
+    public static void main(String[] args) {
+        // Strings
+        String firstName = "Anny";
+        String lastName = "singh";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);  // Anny singh
+    }
+}
+
