c663d6e9a93b69b4e9ac159ee45dc94419aed0f0
annytrix <kumarianjana5801@gmail.com>
2026-06-21 18:46:08 +0530

    package

---
+++ college/Student.java
@@ -0,0 +1,13 @@
+package college;
+
+public class Student {
+    public void print(){
+        System.out.println("College Student");
+    }
+}
+// class Teacher{
+//     public void print(){
+//         Student s1 = new Student();
+//         s1.print();
+//     }
+// }
