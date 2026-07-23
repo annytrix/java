5760881de31990c71eb6868fcde7c0e5807b5df4
annytrix <kumarianjana5801@gmail.com>
2026-07-23 15:31:14 +0530

    payment

---
+++ day 24/demo.java
@@ -0,0 +1,28 @@
+
+public class demo {
+    public static void main(String[] args) {
+        Payment p = new DebitCard();
+        p.pay();
+    }
+}
+
+//polymorphism
+
+interface Payment {
+    void pay();
+}
+
+class CreditCard implements Payment {
+    @Override
+    public void pay() {
+        System.out.println("Paying via credit card");
+    }
+}
+
+class DebitCard implements Payment {
+    @Override
+    public void pay() {
+        System.out.println("Paying via debit card");
+    }
+}
+
---
+++ day 24/demo2.java
@@ -0,0 +1,27 @@
+public class demo2 {
+    
+    public static void main(String[] args) {
+        Payment p = new DebitCard();
+        p.pay();
+    }
+}
+
+//polymorphism
+
+interface Payment {
+    void pay();
+}
+
+class CreditCard implements Payment {
+    @Override
+    public void pay() {
+        System.out.println("Paying via credit card");
+    }
+}
+
+class DebitCard implements Payment {
+    @Override
+    public void pay() {
+        System.out.println("Paying via debit card");
+    }
+}
