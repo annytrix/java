c643fb72197742796d3bd692475449d40ef6a7fb
annytrix <kumarianjana5801@gmail.com>
2026-07-23 15:39:32 +0530

    polymorphism

---
+++ day 24/demo4.java
@@ -0,0 +1,28 @@
+public class demo4 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Interface inheritance
+interface Animal {
+    void eat();
+} 
+
+interface Dog extends Animal {
+    void bark();
+}
+
+class StreetDog implements Dog {
+    
+    @Override
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    @Override
+    public void bark() {
+        System.out.println("Barking");
+    }
+}
